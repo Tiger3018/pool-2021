1dac1d38a4550581c64d99745d1f3a523d8bb91c
Tiger3018 <tiger3018of02@gmail.com>
2021-06-11 03:23:35 +0800

    Basic Circle Completed (Message from client to client)

--- server-flask/app.py
+++ server-flask/app.py
@@ -9,12 +9,15 @@ from pkgMethod import imageProcess
 #     print('Please /run "pip install flask".')
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+# stackoverflow python-jsonify-dictionary-in-utf-8
 CORS(app, resources=r"/api/*")
 
 @app.route("/api/recognition", methods = ['POST'])
 # @cross_origin()
 def api_init_recognition():
-    return imageProcess()
+    dictGet, status_code = imageProcess()
+    return {"Version" : "0.0.1", **dictGet}, status_code
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8888, debug=True)
--- server-flask/pkgMethod.py
+++ server-flask/pkgMethod.py
@@ -1,31 +1,66 @@
 from flask import request
-from requests import get as reqget
-import re, base64, time
+import re, requests, base64, time, hashlib, hmac, json, traceback
 
 def imageProcess():
     try:
         ty1 = request.mimetype
         ty2 = request.files['image'].mimetype
         if ty1 != "multipart/form-data" or not re.match('image/*', ty2):
-            return {"warning": ty1 + ' or ' + ty2 + " not allowed."}, 400
+            return {"Warning": ty1 + ' or ' + ty2 + " not allowed."}, 400
     except NameError:
-        return {"warning": "Empty mimetype (request/id='image') not allowed."}, 400
-    imageObj = request.files['image']
-    # request.files['image'].filename
-    baseObj = base64.b64encode(imageObj.read())
-    tenAPIObj = reqget("https://ocr.tencentcloudapi.com", params={
-		"Action": "GeneralHandwritingOCR",
-		"Region": "ap-guangzhou",
-		"Version": "2018-11-19",
-        "ImageBase64": baseObj,
-        "EnableWordPolygon": True
-        }, headers={
-        "X-TC-Action": "GeneralHandwritingOCR",
-        "X-TC-Region": "ap-guangzhou",
-        "X-TC-Version": "2018-11-19",
-        "X-TC-Timestamp": str(int(time.time()))
-    })
-    print(tenAPIObj.status_code, tenAPIObj.content, len(baseObj) * 3.0 / 4096, sep='\n')
+        return {"Warning": "Empty mimetype (request/id='image') not allowed."}, 400
+
+    imageObj = request.files['image'].read()
+    baseStr = base64.b64encode(imageObj).decode()
+
+    try:
+        with open("tencent.secret", "r") as secretFile:
+            a, b, *_unused = [i.split('\n')[0] for i in secretFile.readlines()]
+            tenAPIObj = tencentProcess(a, b, baseStr)
+        print(tenAPIObj.status_code, tenAPIObj.content.decode(), len(baseStr) * 3.0 / 4096, sep='\n')
+    except FileNotFoundError:
+        return {"Warning": "File tencent.secret Not Found."}, 404
+    except Exception:
+        traceback.print_exception()
+        return {"Warning": "Server's communication with API provider failed.", "Debug": traceback.format_exception()}, 502
     # request.charset
     # request.cookies[]
-    return "200"
+    return json.loads(tenAPIObj.content.decode()), tenAPIObj.status_code
+
+def tencentProcess(seId, seKey, IBData):
+    # gmttime(), json.dumps() instead of str()
+    timeUnix = str(int(time.time()))
+    timeDate = time.strftime("%Y-%m-%d", time.gmtime())
+    method = "/ocr/tc3_request"
+    req = requests.Request("POST", "https://ocr.tencentcloudapi.com",
+        json={"ImageBase64": IBData, "EnableWordPolygon": True},
+        headers={
+            "X-TC-Action": "GeneralHandwritingOCR",
+            "X-TC-Region": "ap-guangzhou",
+            "X-TC-Version": "2018-11-19",
+            "X-TC-Timestamp": timeUnix,
+            "Content-type": "application/json, charset=utf-8",
+            "Host": 'ocr.tencentcloudapi.com'#
+        })
+
+    h1text = ""
+    h2text = ""
+    h3sum = hashlib.sha256(json.dumps(req.json).encode()).hexdigest()
+    for i in ("Content-type", "Host"):
+        h1text += i.lower() + ";"
+        h2text += i.lower() + ":" + req.headers[i].lower() + '\n'
+    h1text = h1text[:len(h1text) - 1]
+    reqStr = "POST\n/\n\n" + h2text + "\n" + h1text + "\n" + h3sum
+    reqStrHashed = hashlib.sha256(reqStr.encode()).hexdigest()
+
+    stringToSign = "TC3-HMAC-SHA256\n" + timeUnix + "\n" + timeDate + method + "\n" + reqStrHashed
+    secretSigning = hmac.new(("TC3" + seKey).encode(), timeDate.encode(), digestmod=hashlib.sha256).digest()
+    for i in re.findall(r"/([^/]*)", method):
+        secretSigning = hmac.new(secretSigning, i.encode(), digestmod=hashlib.sha256).digest()
+    secretSigning = hmac.new(secretSigning, stringToSign.encode(), digestmod=hashlib.sha256).hexdigest()
+
+    req.headers["Authorization"] = "TC3-HMAC-SHA256 Credential=" + seId + "/" + timeDate + method + \
+        ", SignedHeaders=" + h1text + ", Signature=" + secretSigning
+    reqPrepared = req.prepare()
+    print(str(reqPrepared.headers["Authorization"]))
+    return requests.Session().send(reqPrepared)
