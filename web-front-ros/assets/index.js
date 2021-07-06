1daea96c99fb0526c8ed43ce34d66cfa5d806a2c
Tiger3018 <tiger3018of02@gmail.com>
2021-07-06 18:23:25 +0800

    [web-front-ros] connection,subscribe-basic handler

--- web-front-ros/assets/index.js
+++ web-front-ros/assets/index.js
@@ -1,14 +1,17 @@
 "use strict";
 let exposeFunc = {};
 let ros = NaN;
-let rosCnt
+let rosConnectReset = 0;
 let bodyData = NaN; // @see https://dev.to/grrrisu/update-liveview-for-alpine-js-101-41pk
+let formData = NaN;
 
 document.addEventListener('alpine:init', () => {
   Alpine.store('wsList', ["localhost:9090"]);
-});
+})
 
 function alpineGlobalInit(){
+  bodyData = document.body._x_dataStack[0];
+  formData = document.getElementById("task-creator")._x_dataStack[0];
 };
 
 function alpineGlobal(storeMagic){
@@ -17,6 +20,8 @@ function alpineGlobal(storeMagic){
     hostModal: true,
     rosAddress: storeMagic.wsList[0],
     rosStatus: 1,
+    rosErrorCode: 0,
+    rosTask: [],
     ros_error(e) {
       console.log("[ER]"); console.log(e); //console.log(bodyData);
       bodyData.rosStatus = -1;
@@ -47,13 +52,54 @@ function alpineGlobal(storeMagic){
 }
 
 function rosConnectCreate(wsAddress){
+  bodyData.rosAddress = wsAddress;
   bodyData.rosStatus = 0;
-  ros = new ROSLIB.Ros({
-    url : 'ws://' + wsAddress
-  });
-  ros.on("error", bodyData.ros_error);
-  ros.on("connection", bodyData.ros_connection);
-  ros.on("close", bodyData.ros_close);
+  try {
+    ros = new ROSLIB.Ros({
+      url: 'ws://' + wsAddress
+    });
+    ros.on("error", bodyData.ros_error);
+    ros.on("connection", bodyData.ros_connection);
+    ros.on("close", bodyData.ros_close);
+  } catch (error) {
+    bodyData.rosErrorCode = 101;
+    bodyData.ros_error(error);
+  }
+}
+
+function rosTopicCreate (funcData) {
+  //console.log(formData);
+  if(funcData.method == 0) {
+    var subscribeTopic = new ROSLIB.Topic({
+      ros: ros,
+      name: funcData.topic,
+      messageType: funcData.type
+    });
+    var nowAt = bodyData.rosTask.push({
+      action: {
+        start: NaN,
+        pause() {
+          subscribeTopic.unsubscribe()
+        }
+      },
+      message: "Waiting...",
+      roslib: subscribeTopic,
+      topic: funcData.topic,
+      type: funcData.type
+    }) - 1;
+    var rosTaskNow = bodyData.rosTask[nowAt];
+    rosTaskNow.action.start = function() {
+      subscribeTopic.subscribe(function(funcMessage){
+        rosTaskNow.message = funcMessage.msg;
+        console.log(funcMessage);
+      })
+    }
+    rosTaskNow.action.start();
+    console.log(bodyData.rosTask[nowAt]);
+  }
+  else if(funcData.method == 1) {
+
+  }
 }
 /* (function (){
   ros = new ROSLIB.Ros({
@@ -68,7 +114,7 @@ function rosConnectCreate(wsAddress){
 
   });
   */
-var atopic = new ROSLIB.Topic({
+/*var atopic = new ROSLIB.Topic({
   ros: ros, name: "/hrim_actuator_rotaryservo_000000000002/goal_axis1", messageType: "hrim_actuator_rotaryservo_msgs/msg/GoalRotaryServo"
 });
 var amess = new ROSLIB.Message({
@@ -85,3 +131,4 @@ btopic.subscribe(function(message) {
 btopic.unsubscribe();
 });
 // })(); // anonymous code block for publish
+*/
